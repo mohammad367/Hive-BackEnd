04715ab6722d9663a81d51b8dda955abdb07e9e2
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-01-27 21:42:28 +0330

    delete username field from user model

--- core/models.py
+++ core/models.py
@@ -5,4 +5,3 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=255, unique=True)
