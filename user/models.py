b1b0535811551cadf7c0464f42cad60745c140f6
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-01-27 19:58:48 +0330

    add fields of user-model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255, unique=True)
