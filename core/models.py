6251cf8f053a8f7a4eacd107def555e543f75151
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-01-29 20:31:15 +0330

    add USER_NAME='email'  to User model

--- core/models.py
+++ core/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
