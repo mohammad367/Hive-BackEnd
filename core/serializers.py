0334217aff56eab635e053315a0a72804325bb02
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-01-30 19:40:13 +0330

    edit UserVreateSerializer fields

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,7 @@
 from djoser.serializers import UserCreateSerializer as BaseUserSerializer
-
+from djoser.email import ActivationEmail
+#'activation': 'djoser.serializers.ActivationSerializer',
+from djoser.serializers import ActivationSerializer
 class UserCreateSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         fields=['id','email','username','password','first_name','last_name']
