483043dd34c94b20185f5fcf4c80471014285716
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-01-30 19:51:31 +0330

    email config

--- hive_backend/settings.py
+++ hive_backend/settings.py
@@ -153,9 +153,9 @@ DJOSER = {
     }
 }
 
-# EMAIL_BACKEND = 'django.core.mail.backend.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = os.environ('MAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ('MAIL_HOST_PASSWORD')
-# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backend.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ('MAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ('MAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
