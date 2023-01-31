0420aa66240b0f6da34da447f7144065730c4a8d
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-01-31 12:55:33 +0330

    todo list added

--- charity/models.py
+++ charity/models.py
@@ -6,3 +6,23 @@ from django.db import models
 # advertise model
 
 # donatordonatee model
+from django.db import models
+
+
+class Advertisement(models.Model):
+    pass
+# amount
+# collected amount
+# raiser -> foreignkey: DonatorDonatee
+# title
+# description
+
+
+class DonatoDonatee(models.Model):
+    pass
+# birth_date
+# Identity_card_number
+# Identity_card_photo
+# Identity_card_photo1
+# account_number
+# user -> content_type: Core_User
