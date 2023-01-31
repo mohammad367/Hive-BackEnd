9f3f3f5dc1bb6a6904188325bff8a7f03ec37490
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2023-02-01 01:08:01 +0330

    add serializer to chrity

--- charity/serializers.py
+++ charity/serializers.py
@@ -1,19 +1,39 @@
 from rest_framework import serializers
-
+from charity.models import Advertisement , DonatorDonate
 
 class AdvertisementSerializer(serializers.ModelSerializer):
-    pass
-# title
+    '''getting list of advertimsements and creating post new advertisement'''
+    serializer_class = AdvertisementSerializer
+    def get(self,request):
+        '''retreive a list of ads'''
+        if request.method == "GET":
+            ads = Advertisement.objects.get(pk = id)
+            serializer = AdvertisementSerializer(ads, many=True)
+            return Response(serializer.data)
+        def post(self,request):
+            '''creating a new ad with provided data'''
+            serializer = AdvertisementSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+    
 # description
 # raiser
 # amount
 # collected_amount
 # ...
 
-
-class DonatorDonateeSerializer(serializers.ModelSerializer):
-    pass
-# birth_date
-# account_number
-# photo??
-# ...
+class DonatorDonateSerializer(serializers.ModelSerializer):
+    serializer_class = DonatorDonateSerializer
+    def get(self,request):
+        
+        if request.method == "GET":
+            donator = DonatorDonate.objects.get(pk = id)
+            serializer = DonatorDonateSerializer(donator, many=True)
+            return Response(serializer.data)
+        def post(self,request):
+            
+            donator = DonatorDonateSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
