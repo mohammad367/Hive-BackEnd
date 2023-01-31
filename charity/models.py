a9442a89fa250c0ffcf5877171fabc090e5a4e48
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2023-02-01 00:32:08 +0330

    fix syntax problems

--- charity/models.py
+++ charity/models.py
@@ -7,9 +7,8 @@ class Advertisement(models.Model):
     raiser = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-    amout = models.FloatField()
-    collected_amount = models.FloatField()
     amount = models.FloatField()
+    collected_amount = models.FloatField()
     collected_amount = models.FloatField()
 
 
@@ -17,7 +16,6 @@ class DonatorDonate(models.Model):
     donator = models.ForeignKey(User, on_delete=models.PROTECT)
     account_number = models.CharField(max_length=16)
     birth_date = models.DateField()
-    account_number = models.CharField(max_length=16)
     identity_card_number = models.CharField(max_length=10)
     identity_card_image = models.ImageField()
     birth_certificate_image = models.ImageField()
