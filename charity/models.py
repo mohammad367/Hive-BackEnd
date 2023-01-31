3fdff3f4748485cc51d90724afe42ef577aa625f
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2023-02-01 00:17:25 +0330

    model migrations

--- charity/models.py
+++ charity/models.py
@@ -1,34 +1,23 @@
 from django.db import models
-
+from core.models import User
 # Create your models here.
 
 
-# advertise model
-
-# donatordonatee model
-from django.db import models
-
-
 class Advertisement(models.Model):
+    raiser = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-    raiser = models.ForeignKey()
-    amout = models.CharField()
-    collected_amount = models.CharField()
-   
+    amout = models.FloatField()
+    collected_amount = models.FloatField()
     amount = models.FloatField()
     collected_amount = models.FloatField()
-    raiser = models.ForeignKey(to, on_delete)
 
-class DonatoDonatee(models.Model):
+
+class DonatorDonate(models.Model):
+    donator = models.ForeignKey(User, on_delete=models.PROTECT)
     account_number = models.CharField(max_length=16)
-    identity_card_number = models.CharField()
-    identity_card_image = models.ImageField()
-    identity_card_image2 = models.ImageField()
-
     birth_date = models.DateField()
-    Identity_card_number = models.CharField(max_length=10)
-    Identity_card_photo = models.ImageField()
-    Identity_card_photo1 = models.ImageField()
     account_number = models.CharField(max_length=16)
-# user -> content_type: Core_User
+    identity_card_number = models.CharField(max_length=10)
+    identity_card_image = models.ImageField()
+    birth_certificate_image = models.ImageField()
