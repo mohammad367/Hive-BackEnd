4ce258171abd1ac49d943d739b53e7612d201c74
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-01-31 15:27:42 +0330

    create UserProfileSerializer and AdvertisementSerializer

---
+++ charity/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    pass
+# title
+# description
+# raiser
+# amount
+# collected_amount
+# ...
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    pass
+# birth_date
+# account_number
+# photo??
+# ...
