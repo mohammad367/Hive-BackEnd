636b17f83957256000cfd3334525863a3f417df6
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2023-02-03 00:44:32 +0330

    add category to advertisment

--- charity/models.py
+++ charity/models.py
@@ -6,11 +6,10 @@ from core.models import User
 
 class DonatorDonate(models.Model):
     # -> use content type instead of import user from Core App
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    donator = models.ForeignKey(User, on_delete=models.PROTECT)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     account_number = models.CharField(max_length=16)
-    # category ->choice field
     birth_date = models.DateField()
     identity_card_number = models.CharField(max_length=10)
     identity_card_image = models.ImageField()
@@ -21,5 +20,17 @@ class Advertisement(models.Model):
     raiser = models.ForeignKey(DonatorDonate, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    categories = [
+        (1,'general'),
+        (2,'animals'),
+        (3,'medicine'),
+        (4,'education'),
+        (5,'family'),
+        (6,'natural_disaster'),
+        (7,'human_catastrophe'),
+        (8,'businesses'),
+        (9,'environment')
+    ]
+    category = models.CharField(max_length=30,choices=categories, default=1)
     amount = models.FloatField()
     collected_amount = models.FloatField()
