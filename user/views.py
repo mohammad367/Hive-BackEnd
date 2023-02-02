6c5291062493f66ee3fd5825ddc20e24b1620779
SepehrNorouzi7 <snorouzi07@gmail.com>
2023-02-02 16:49:29 +0330

    Add some comments
    extend schema comment added

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+"""
+    @extend_schema(
+        summary="Create a new user",
+        responses={
+            201: OpenApiResponse(response=SignupSerializer,
+                                 description='Created.'),
+            400: OpenApiResponse(description='bad request, user exist or you have to make sure you fill the necessary fields correctly'),
+        },
+    )
+
+"""
+
+# def post
+
+# def create
+
+# ...
