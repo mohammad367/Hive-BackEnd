e25fb07e54dbc53d75d8726d2647db97f69ed63f
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-02-03 21:20:06 +0330

    add profile endpoint

--- charity/urls.py
+++ charity/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 routers = routers.DefaultRouter()
 routers.register(r'advertisements', views.AdvertisementViewSet,
                  basename='advertisements')
-
+routers.register(r'profile', views.UserProfileSerializer, basename='profile')
 
 urlpatterns = [
     path('', include(routers.urls))
