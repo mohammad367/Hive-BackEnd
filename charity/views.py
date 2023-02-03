46abfadd1e88db86f5cb5b2ab1bdbb15027076fd
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-02-03 23:22:03 +0330

    add me action to DonatorDonateeViewSet and change permission to IsAdmin

--- charity/views.py
+++ charity/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
 from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Advertisement, DonatorDonate
 from .serializers import AdvertisementSerializer, UserProfileSerializer
 
@@ -13,8 +17,24 @@ class AdvertisementViewSet(ModelViewSet):
 class DonatorDonateeViewSet(ModelViewSet):
     queryset = DonatorDonate.objects.all()
     serializer_class = UserProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
 
-    @classmethod
-    def user_test(self):
-        print(self.request.user.user_permissions)
+    # def get_permissions(self):
+    #     if self.request.method in permissions.SAFE_METHODS:
+    #         return permissions.IsAuthenticated()
+
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[permissions.IsAuthenticated])
+    def me(self, request: HttpRequest):
+
+        (profile, created) = DonatorDonate.objects.get_or_create(
+            user_id=request.user.id
+        )
+        print('hi\n\n\n')
+        if request.method == 'GET':
+            serializer = UserProfileSerializer(profile)
+        elif request.method == 'PUT':
+            serializer = UserProfileSerializer(profile, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+        return Response(serializer.data)
