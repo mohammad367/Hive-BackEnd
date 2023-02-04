881b56a67e5ebcedfebe527e756f195cc9d44159
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-02-04 11:54:43 +0330

    delete all migrations

--- charity/serializers.py
+++ charity/serializers.py
@@ -3,7 +3,13 @@ from charity.models import Advertisement, Profile
 
 
 class AdvertisementSerializer(serializers.ModelSerializer):
-    raiser_id = serializers.IntegerField()
+    raiser_id = serializers.SerializerMethodField(
+        read_only=True, method_name='get_raiser_id')
+    print('\n\n\nhi\n\n\n')
+
+    def get_raiser_id(self):
+        print('hi\n\n\n\n')
+        return self.context['raiser_id']
 
     class Meta:
         model = Advertisement
@@ -13,10 +19,11 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
 class UserProfileSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    user_id = serializers.IntegerField(read_only=True)
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True)
 
     class Meta:
         model = Profile
         # add all fields included in user profile design
-        fields = ['id', 'user_id', 'first_name', 'last_name',
+        fields = ['id', 'user', 'first_name', 'last_name',
                   'account_number', 'birth_date', 'identity_card_image']
--- charity/views.py
+++ charity/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
+from rest_framework import mixins
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -13,15 +13,38 @@ class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
 
+    def get_serializer_context(self):
+        request = self.request
+        user_id = request.user.id
+        try:
+            raiser_id = Profile.objects.get(user_id=user_id)
+            print('\n\n\n')
+            print(raiser_id)
+            print('\n\n\n')
+        except:
+            pass
+        return {"raiser_id": raiser_id}
 
-class ProfileViewSet(ModelViewSet):
+
+class ProfileViewSet(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     GenericViewSet):
     queryset = Profile.objects.all()
     serializer_class = UserProfileSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_context(self):
+        return {"user_id": self.request.user.id}
 
     # def get_permissions(self):
     #     if self.request.method in permissions.SAFE_METHODS:
-    #         return permissions.IsAuthenticated()
+    #         return [permissions.IsAuthenticated()]
+    #     elif self.request.method == 'POST':
+    #         return [permissions.IsAuthenticated()]
+    #     else:
+    #         return [permissions.IsAdminUser()]
 
     @action(detail=False, methods=['GET', 'PUT'], permission_classes=[permissions.IsAuthenticated])
     def me(self, request: HttpRequest):
@@ -29,7 +52,7 @@ class ProfileViewSet(ModelViewSet):
         (profile, created) = Profile.objects.get_or_create(
             user_id=request.user.id
         )
-        print('hi\n\n\n')
+
         if request.method == 'GET':
             serializer = UserProfileSerializer(profile)
         elif request.method == 'PUT':
