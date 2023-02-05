ce10cbc87b4fa519ada5b168aff0f1368a4ebabd
Mohammad Nasri <nasrimohammad367@gmail.com>
2023-02-05 19:41:42 +0330

    add 'me' action to profileViewSet and check if the user have profile or not

--- charity/views.py
+++ charity/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework import mixins
-from rest_framework import permissions
+from rest_framework import mixins, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Advertisement, Profile
@@ -14,18 +13,16 @@ class AdvertisementViewSet(ModelViewSet):
     serializer_class = AdvertisementSerializer
 
     def get_serializer_context(self):
-        return {"request": self.request}
-    # def get_serializer_context(self):
-    #     request = self.request
-    #     user_id = request.user.id
-    #     try:
-    #         raiser_id = Profile.objects.get(user_id=user_id)
-    #         print('\n\n\n')
-    #         print(raiser_id)
-    #         print('\n\n\n')
-    #     except:
-    #         pass
-    #     return {"raiser_id": raiser_id}
+        request = self.request
+        user_id = request.user.id
+        try:
+            raiser_id = Profile.objects.get(user_id=user_id)
+            print('\n\n\n')
+            print(raiser_id)
+            print('\n\n\n')
+        except:
+            pass
+        return {"raiser_id": raiser_id}
 
 
 class ProfileViewSet(mixins.CreateModelMixin,
@@ -35,6 +32,28 @@ class ProfileViewSet(mixins.CreateModelMixin,
                      GenericViewSet):
     queryset = Profile.objects.all()
     serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_serializer_context(self):
-        return {'request': self.request}
+        return {"request": self.request}
+
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[permissions.IsAuthenticated])
+    def me(self, request: HttpRequest):
+        try:
+            (profile, created) = Profile.objects.get_or_create(
+                user_id=request.user.id
+            )
+        except:
+            # To do :serialize a message to client
+
+            return Response(
+                'this user does not have a profile', status=status.HTTP_404_NOT_FOUND)
+
+        if request.method == 'GET':
+            serializer = UserProfileSerializer(profile)
+        elif request.method == 'PUT':
+            serializer = UserProfileSerializer(profile, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+        return Response(serializer.data)
