89dde503fd743d1876bcb6887e92020cd9832087
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2023-02-08 18:53:35 +0330

    complete profile photos and ad photo

--- charity/models.py
+++ charity/models.py
@@ -10,16 +10,18 @@ class DonatorDonate(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     account_number = models.CharField(max_length=16)
+    avatar = models.ImageField(upload_to = 'profile/avatar/')
     birth_date = models.DateField()
     identity_card_number = models.CharField(max_length=10)
-    identity_card_image = models.ImageField()
-    birth_certificate_image = models.ImageField()
+    identity_card_image = models.ImageField(upload_to = 'profile/identity_card_image/')
+    birth_certificate_image = models.ImageField(upload_to = 'profile/birth_certificate_image/')
 
 
 class Advertisement(models.Model):
     raiser = models.ForeignKey(DonatorDonate, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to = 'ads/')
     categories = [
         ('G','general'),
         ('A','animals'),
