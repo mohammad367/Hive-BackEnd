7cd2674314a8816ae2801dfe24e1ae356311a6e7
Mohammad Hussein Asnavandi <79707159+mhas1381@users.noreply.github.com>
2023-02-13 20:10:57 +0330

    customize charity admin panel

--- charity/admin.py
+++ charity/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from charity.models import Advertisement,DonatorDonate
 
 # Register your models here.
+class DonatorDonateAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    list_display = ('first_name' ,'last_name')
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty'
+    list_display = ('raiser' ,'title' ,  'description' , 'category','amount' ,'collected_amount' )
+    list_filter = ('title' ,'category')
+    search_fields = ['title' , 'raiser' , 'amount' , 'collected_amount']
+
+admin.site.register(DonatorDonate , DonatorDonateAdmin)
+admin.site.register(Advertisement,AdvertisementAdmin)
